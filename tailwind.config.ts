9d34c6439cf37b8964131c4634ec69a387ad53ee
eskim <eskim@acrofuture.com>
2023-08-16 17:51:55 +0900

    tailwind css 설정 작업중

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,20 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}'
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        require('@tailwindcss/nesting'),
+        require('postcss-import'),
+        require('postcss-nested')
+    ],
 }
 export default config
