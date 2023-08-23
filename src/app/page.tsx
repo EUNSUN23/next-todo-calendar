0b2c1a0aec38776346be085984d0f6f44e09cae8
eskim <eskim@acrofuture.com>
2023-08-23 14:16:39 +0900

    Design: 1. todo영역 폰트 수정 2. d-day Badge추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "@/components/Header";
+import Badge from "@/components/Badge";
 
 
 export default function Home() {
@@ -7,21 +8,34 @@ export default function Home() {
         <>
             <div className="max-w-[150rem] my-20 mx-auto bg-[var(--color-grey-light-2)] shadow-[var(--shadow-dark)] min-h-[50rem]">
                 <Header/>
-                <section className="flex flex-wrap">
-                    <section className='border border-black basis-full md:basis-1/3 lg:basis-1/4'>
-                        <section className='flex w-full'>
-                            <h2 className='text-3xl'>오늘의 TO DO</h2>
-                            <span className='text-2xl self-end border border-red'>2023년 8월 22일 화요일</span>
+                    <main className="flex flex-wrap">
+                        {/* todo 시작 */}
+                        <section className='border border-black basis-full md:basis-1/3 lg:basis-1/4 '>
+                            {/*todo 타이틀 시작*/}
+                            <section className='flex justify-between w-full py-4 px-7'>
+                                <div className='text-3xl font-semibold text-[var(--color-gray-dark-3)]'>오늘의 TO DO</div>
+                                <span className='text-2xl font-semibold text-[var(--color-gray-dark)]'>2023년 8월 22일 화요일</span>
+                            </section>
+                            {/*todo 타이틀 끝*/}
+                            {/*todo 목록 시작*/}
+                            <section className='description'>
+                                <ul>
+                                    <li className='text-2xl pl-4 text-stone-600 font-medium'>
+                                        ▪ 프로젝트 레이아웃 구성 🗒️
+                                        <Badge color='red' size='sm' text='D-5'/>
+                                    </li>
+                                </ul>
+                            </section>
+                            {/*todo 목록 끝*/}
                         </section>
-                        <div className='description'>
+                        {/* todo 끝 */}
+                        {/*calendar 시작*/}
+                        <section className='flex-1'>
+                            <h3>content 1</h3>
 
-                        </div>
-
-                    </section>
-                    <main className="flex-1">
-                        <h3>content 1</h3>
+                        </section>
+                        {/*calendar 끝*/}
                     </main>
-                </section>
             </div>
         </>
     );
---
+++ src/components/Badge.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+type Props = {
+    color: string;
+    size: string;
+    text: string;
+};
+
+function Badge({color, size, text}:Props) {
+    // 사이즈
+    let textSize;
+    let px;
+    let py;
+    switch(size){
+        case 'sm':
+            textSize = 'text-sm';
+            px = 'px-4';
+            py = 'py-1';
+            break;
+        case 'md':
+            textSize = 'text-md';
+            px = 'px-4';
+            py = 'py-2';
+            break;
+        case 'lg':
+            textSize = 'text-lg';
+            px = 'px-8';
+            py = 'py-4';
+            break;
+        default:
+            textSize = 'text-xs';
+            px = 'px-2';
+            py = 'py-1';
+    }
+
+    // 색상
+    let bgColor;
+    let textColor;
+    let ringColor;
+    switch(color){
+        case 'red':
+            bgColor = 'bg-red-50';
+            textColor = 'text-red-700';
+            ringColor = 'ring-red-600/10';
+            break;
+        case 'yellow':
+            bgColor = 'bg-yellow-50';
+            textColor = 'text-yellow-800';
+            ringColor = 'ring-yellow-600/20';
+            break;
+        case 'green':
+            bgColor = 'bg-green-50';
+            textColor = 'text-green-700';
+            ringColor = 'ring-green-600/20';
+            break;
+        default:
+            // todo - error 던지기?
+            bgColor = 'bg-red-50';
+            textColor = 'text-red-700';
+            ringColor = 'ring-red-600/10';
+            break;
+    }
+
+
+    return (
+        <span className={`badge-rounded-md ${bgColor} ${textColor} ${ringColor} ${textSize} ${px} ${py}`}>{text}</span>
+    );
+}
+
+export default Badge;
