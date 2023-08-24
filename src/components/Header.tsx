912483913e402b828b002f7b10c8b0113e2ef7bb
eskim <eskim@acrofuture.com>
2023-08-24 17:25:12 +0900

    Design: [body] background-shadow 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ import React from 'react';
 
 function Header() {
     return (
-        <header className="flex justify-between items-center text-2xl h-28 bg-white border-b border-[var(--line)]">
+        <header className="flex justify-between items-center text-2xl h-28 bg-white">
             <nav className="user-nav">
                 <div className="user-nav__user">
                     <span className="text-[var(--color-gray-dark-2)]">Eunsun</span>
