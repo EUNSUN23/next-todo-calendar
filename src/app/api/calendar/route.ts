5d8c8e7339f962600e1a87d6fe575d3054a6c094
eunsun <iso9864@naver.com>
2023-08-26 19:31:22 +0900

    Feature:

--- src/app/api/calendar/route.ts
+++ src/app/api/calendar/route.ts
@@ -4,5 +4,6 @@ import {getEvents} from "@/service/calendar";
 // /products api호출시 제품 목록 조회
 export async function GET(request:Request){
     const events = await getEvents();
+    console.log("events: ",events);
     return NextResponse.json({events});
 }
--- src/components/CalendarSection.tsx
+++ src/components/CalendarSection.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import Calendar from "@/components/Calendar";
 // import Calendar from "@/components/Calendar";
 
 async function CalendarSection() {
     // todo 이번달에 해당하는 모든 event 조회해서 Calendar컴포넌트에 넘겨주기.
-    const events = await fetch("http://localhost:3000/api/calendar");
+    const res = await fetch("/api/calendar");
+    const data = await res.json();
 
-    console.log("res: ",events);
+    console.log("data: ",data);
 
     return (
         <section className='flex-1'>
-            {/*<Calendar events={}/>*/}
+            <Calendar events={data.events}/>
         </section>
     );
 }
