0071d1eefe8dc31ffe370626e4dd77f17a8d5c44
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-06 13:45:50 +0900

    Feature: 공통 type.tsx 추가

---
+++ src/utils/types.ts
@@ -0,0 +1,26 @@
+export type Note = {
+    id: string;
+    groupId: string;
+    contents: string;
+}
+
+export type Todo = Note & {finish:boolean;}
+
+export type User = {
+    name:string;
+    userName:string;
+    phone:string;
+    createdAt:string;
+}
+
+export type Task = {
+    groupId:string;
+    description: string;
+    start: string;
+    end: string;
+    level: string;
+    finish: boolean;
+    notes:Note[] | [];
+    todos:Todo[] | [];
+    createdBy: User | Record<string, never>;
+}
