a8b03dddadcabe421273000322d5f9cc2056375b
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-06 10:18:36 +0900

    Feature: 안 쓰는 코드 정리

--- src/app/api/calendar/route.ts
+++ src/app/api/calendar/route.ts
@@ -1,8 +1,7 @@
 import {NextResponse} from "next/server";
-import {getEvents, getTasks} from "@/service/calendar";
+import {getTasks} from "@/service/calendar";
 
 export async function GET(request:Request){
-    // const events = await getEvents();
     const tasks = await getTasks();
     return NextResponse.json({tasks});
 }
--- src/service/calendar.ts
+++ src/service/calendar.ts
@@ -1,21 +1,5 @@
 import {ClientConfig, createClient} from "next-sanity";
-import path from "path";
-import {readFile} from "fs/promises";
-import _ from "lodash";
 import {DateFormat, formatDateToStr} from "@/utils/common";
-
-export type CalendarEvent = {
-    id: string;
-    title: string;
-    start: string;
-    end: string;
-    url: string;
-    className: string;
-    color: string;
-    backGroundColor: string;
-    borderColor: string;
-    textColor: string;
-}
 
 const client = createClient({
     projectId: "1jtekrqc",
@@ -23,19 +7,6 @@ const client = createClient({
     apiVersion: "2022-03-25",
     useCdn: false
 } as ClientConfig);
-
-export async function getEvents(){
-    // 이번달에 start하는 이벤트 조회
-    // todo 서버에서 데이터 조회해올것.
-    const today = new Date();
-    const year = today.getFullYear().toString();
-    const month = _.padStart((today.getMonth() + 1).toString(),2,"0");
-
-    const filePath = path.join(process.cwd(), 'data', 'events.json');
-    return readFile(filePath, 'utf-8')
-        .then<CalendarEvent[]>(JSON.parse)
-        .then((events) => events.filter(event => event.start.substring(0,4) === year && event.start.substring(4,6) === month));
-}
 
 export async function getTasks(){
      const date = new Date();
