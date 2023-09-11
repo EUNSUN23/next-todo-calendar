c206f07cae319293d84ef8ffde77ac6526e62e4e
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-11 22:36:11 +0900

    Feature: Todo onClick 메서드 추가, useCurrentTask hook사용

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,16 +3,18 @@ import React from 'react';
 import {Task} from "@/utils/types";
 import CheckSquare from "@/components/CheckSquare";
 import Badge from "@/components/Badge";
+import {useCurrentTask} from "@/context/CurrentTaskContext";
 
 type Props = {
     task:Task;
 }
 function Todo({task}:Props) {
+    const {openCurrentTask, closeCurrentTask, isOpen} = useCurrentTask();
     console.log("task: ",task);
     // todo - description클릭시 task 상세 페이지 open..
     return (
-        <li key={task.groupId} className='flex justify-center space-x-4 text-2xl text-[var(--color-gray-dark)] font-semibold'>
-              <span className='flex'>
+        <li className='flex justify-center space-x-4 text-2xl text-[var(--color-gray-dark)] font-semibold'>
+              <span className='flex pointer' onClick={() => isOpen ? closeCurrentTask() : openCurrentTask(task)}>
                   <CheckSquare id='todo1'/>
                   <span>{task.description}</span>
               </span>
