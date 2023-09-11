14db17ec99755b46d21d8d010d8fb76338d8e036
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-11 14:46:59 +0900

    Feature: Task조회 useQuery -> 커스텀훅 useTasks로 분리

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -9,12 +9,13 @@ type Props = {
 }
 function Todo({task}:Props) {
     console.log("task: ",task);
+    // todo - description클릭시 task 상세 페이지 open..
     return (
-        <li className='flex justify-center space-x-4 text-2xl text-[var(--color-gray-dark)] font-semibold'>
-                              <span className='flex'>
-                                  <CheckSquare id='todo1'/>
-                                  {task.description}
-                              </span>
+        <li key={task.groupId} className='flex justify-center space-x-4 text-2xl text-[var(--color-gray-dark)] font-semibold'>
+              <span className='flex'>
+                  <CheckSquare id='todo1'/>
+                  <span>{task.description}</span>
+              </span>
             <Badge color='red' size='sm' text='D-5'/>
         </li>
     );
--- src/components/TodoSection.tsx
+++ src/components/TodoSection.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import TodoHeader from "@/components/TodoHeader";
 import TodoLists from "@/components/TodoLists";
+import calendarSection from "@/components/CalendarSection";
 
 
 function TodoSection() {
     return (
-        <section className='basis-full md:basis-1/3 lg:basis-1/4 '>
-            <TodoHeader/>
-            <TodoLists/>
-        </section>
+        <>
+            <section className='basis-full lg:basis-1/5 '>
+                <TodoHeader/>
+                <TodoLists/>
+            </section>
+            <section className='border-2 border-black basis-full md:basis-1/3 lg:basis-1/4'>
+
+            </section>
+        </>
     );
 }
 
