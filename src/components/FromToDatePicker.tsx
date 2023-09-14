36a1f4bbb3f71dcaeff172d4d97859d85ccccaff
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-14 12:00:33 +0900

    Feature: FromToDatePicker 컴포넌트 추가

---
+++ src/components/FromToDatePicker.tsx
@@ -0,0 +1,30 @@
+'use client';
+import React, {useState} from 'react';
+import DatePicker from "react-datepicker";
+import {BsCalendarWeek} from "react-icons/bs";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+type Props = {
+    startDate: Date;
+    endDate: Date;
+    onChangeStartDate: () => void;
+    onChangeEndDate : () => void;
+}
+function FromToDatePicker({startDate, endDate, onChangeStartDate, onChangeEndDate}:Props) {
+    return (
+        <section className='px-1.5 text-2xl text-[var(--color-gray-dark-3)]'>
+            <span>
+                <DatePicker selected={startDate} onChange={onChangeStartDate}/>
+                <BsCalendarWeek/>
+            </span>
+            <span>&#45;</span>
+            <span>
+                <DatePicker selected={endDate} onChange={onChangeEndDate}/>
+                <BsCalendarWeek/>
+            </span>
+        </section>
+    );
+}
+
+export default FromToDatePicker;
