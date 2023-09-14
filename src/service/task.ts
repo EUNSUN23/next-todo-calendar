5093a958b7d6297f39a635e848a3619e1ef37727
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-14 12:50:29 +0900

    Feature: task.tsx 수정 - notes, todos, createdBy등 상세정보는 getTask 메서드 통해 조회하도록 수정

--- src/service/task.ts
+++ src/service/task.ts
@@ -8,14 +8,31 @@ const client = createClient({
     useCdn: false
 } as ClientConfig);
 
+/**
+ * groupId로 task 상세 조회
+ * @param taskId
+ */
+
+export async function getTaskById(taskId:string){
+    const query = `*[_type == "task" && groupId == ${taskId}]{
+        groupId, description, start, end, level, finish
+        notes[]->, todos[]->, createdBy->
+    }`;
+
+    return await client.fetch(query);
+}
+
+
+/**
+ * 이번달 task 목록 조회
+ */
 export async function getTasks(){
      const date = new Date();
      const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
      const endDate = formatDateToStr(lastDayOfMonth, DateFormat.YMD_DASH);
 
     const query = `*[_type == "task" && start <= "${endDate} && !finish"]{
-        groupId, description, start, end, level, finish,
-        notes[]->, todos[]->, createdBy->
+        groupId, description, start, end, level, finish
     }`;
     const res = await client.fetch(query);
 
