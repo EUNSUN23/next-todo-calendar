ae6fa9b9f0af5de7a90f080e7cd9c58bc94687c1
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-15 22:24:18 +0900

    Feature: TaskDetailView.tsx > 시작/종료 날짜 onChangeHandler 추가

--- src/components/TaskDetailView.tsx
+++ src/components/TaskDetailView.tsx
@@ -2,18 +2,23 @@
 import React from 'react';
 import FromToDatePicker from "@/components/FromToDatePicker";
 import {useTask} from "@/hooks/useTask";
+import {useEditTask} from "@/hooks/useEditTask";
+import {formatDateToStr} from "@/utils/common";
+import {DateFormat} from "@/utils/constant";
 
 type Props = {
     currentTaskId: string;
 }
-function TaskDetailView({currentTaskId}:Props) {
-    const {task, isLoading, error} = useTask(currentTaskId);
 
-    if(isLoading) return <div>loading...</div>; // todo - loader 추가
+function TaskDetailView({currentTaskId}: Props) {
+    const {task, isLoading, error} = useTask(currentTaskId);
+    const {isEditing, editTaskById} = useEditTask();
+
+    if (isLoading) return <div>loading...</div>; // todo - loader 추가
     // todo react-query로 taskId에 맞는 task 가져오기.
 
 
-    console.log("currentTask: ",task);
+    console.log("currentTask: ", task);
     // currentTask.title // 테스트프로젝트 2 /타이틀
     // currentTask.finish // 테스트프로젝트 2 / 완료 여부
     // currentTask.start // yyyy-mm-dd
@@ -23,16 +28,25 @@ function TaskDetailView({currentTaskId}:Props) {
     // currentTask.todos // todo목록 (contents:내용, finish, groupId:속한 taskid, id: 고유id (groupId + 자기id), createdBy, updatedDate, assignee
     // currentTask.notes // notes목록 (contents:내용, groupId:속한 taskid, id: 고유id (groupId + 자기id), createdBy, updatedDate
     // currentTask.createdBy
+    function onChangeStartDateHandler(date:Date) {
+        console.log("start value::: ",date);
+        editTaskById({...task, start: formatDateToStr(date, DateFormat.YMD_DASH)});
+    }
+
+    function onChangeEndDateHandler(date:Date){
+        console.log("end value::: ",date);
+        editTaskById({...task, end: formatDateToStr(date, DateFormat.YMD_DASH)});
+    }
 
     return (
         <section className='w-full border-1 border-black p-3'>
             <section className='w-full flex'>
-                <span className='text-3xl font-semibold text-[var(--color-gray-dark-3)]'>테스트 프로젝트 2</span>
+                <span className='text-3xl font-semibold text-[var(--color-gray-dark-3)]'>{task.description}</span>
                 <FromToDatePicker
                     startDate={new Date(task.start)}
                     endDate={new Date(task.end)}
-                    onChangeStartDate={() => {}}
-                    onChangeEndDate={() => {}}
+                    onChangeStartDate={onChangeStartDateHandler}
+                    onChangeEndDate={onChangeEndDateHandler}
                 />
             </section>
         </section>
