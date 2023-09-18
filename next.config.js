b2e5731e6c7439eae714615a17f2edda5bdf8cc4
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-18 22:20:33 +0900

    Feature: Avatar.tsx 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com'
+            }
+        ]
+    }
+}
 
+// eslint-disable-next-line no-undef
 module.exports = nextConfig
