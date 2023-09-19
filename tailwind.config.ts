265f226c413e4e38cfa7f41403a5fbaf3e3b4184
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-19 21:04:13 +0900

    Design(tailwind.config.ts): hover-neutral-light-1 유틸리티 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type {Config} from 'tailwindcss'
+import plugin from "tailwindcss/plugin";
+require('tailwindcss/plugin');
 
 const config: Config = {
     content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html", "./app/**/*.{js,jsx,ts,tsx}", "./index.html"],
@@ -15,7 +17,23 @@ const config: Config = {
         require('postcss-import'),
         require('tailwindcss/nesting'),
         require('tailwindcss'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        plugin(function ({addComponents}){
+            addComponents({
+                '.hover-neutral-light-1':{
+                    border:'1px solid white',
+                    borderRadius:'0.25rem',
+                    '&:hover':{
+                        border:'1px solid #EEF1F5',
+                        borderRadius:'0.25rem',
+                        backgroundColor:'#EEF1F5',
+                        transitionTimingFunction: 'transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1)',
+                        transitionDuration:'120ms',
+                        transitionProperty:'background-color'
+                    }
+                }
+            })
+        })
     ],
 }
 export default config
