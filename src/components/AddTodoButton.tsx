3c65965acaaaad7a441308b6824cf7786d869e6a
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-19 18:59:34 +0900

    Design(AddTodoButton.tsx): padding수정

--- src/components/AddTodoButton.tsx
+++ src/components/AddTodoButton.tsx
@@ -3,7 +3,7 @@ import {RiAddLine} from "react-icons/ri";
 
 function AddTodoButton() {
     return (
-        <button className='w-full flex items-center space-x-1 pl-4 pr-2 py-2 text-3xl text-[var(--color-neutral-dark)] hover:bg-[var(--color-neutral-light-1)] hover:border hover:border-[var(--color-neutral-light-1)] hover:rounded hover:m-[-1px]'>
+        <button className='w-full flex items-center space-x-1 pl-8 py-2 text-3xl text-[var(--color-neutral-dark)] hover:bg-[var(--color-neutral-light-1)] hover:border hover:border-[var(--color-neutral-light-1)] hover:rounded hover:m-[-1px]'>
             <RiAddLine />
             <span>업무 목록 추가</span>
         </button>
