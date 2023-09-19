ea484cf52f44ec263d5b1f18eda88ce098718545
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-19 21:06:16 +0900

    Design: hover-neutral-light-1 클래스 적용

--- src/components/AddTodoButton.tsx
+++ src/components/AddTodoButton.tsx
@@ -3,7 +3,7 @@ import {RiAddLine} from "react-icons/ri";
 
 function AddTodoButton() {
     return (
-        <button className='w-full flex items-center space-x-1 py-2 px-1 text-[1.65rem] leading-[2.15rem] text-[var(--color-neutral-dark)] hover:bg-[var(--color-neutral-light-1)] hover:border hover:border-[var(--color-neutral-light-1)] hover:rounded hover:m-[-1px]'>
+        <button className='w-full flex items-center space-x-1 py-2 px-1 text-[1.65rem] leading-[2.15rem] text-[var(--color-neutral-dark)] hover-neutral-light-1'>
             <RiAddLine />
             <span>업무 목록 추가</span>
         </button>
