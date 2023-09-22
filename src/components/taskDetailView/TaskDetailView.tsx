da8cc090cb0c15932373f6e7dae5d78e5801245b
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-22 15:26:38 +0900

    Refactor(TaskDetailView.tsx): TaskTodoAddForm, TaskTodoAddButton 한 컴포넌트(TaskTodoAdd.tsx)로 병합

--- src/components/taskDetailView/TaskDetailView.tsx
+++ src/components/taskDetailView/TaskDetailView.tsx
@@ -3,11 +3,10 @@ import React from 'react';
 import {useTask} from "@/hooks/useTask";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import Avatar from "@/components/Avatar";
-import TaskTodoAddButton from "@/components/taskDetailView/taskTodoAdd/TaskTodoAddButton";
-import TaskTodoAddForm from "@/components/taskDetailView/taskTodoAdd/TaskTodoAddForm";
 import TaskTodoFilter from "@/components/taskDetailView/TaskTodoFilter";
 import TaskDetailViewHeader from "@/components/taskDetailView/TaskDetailViewHeader";
+import TaskMember from "@/components/taskDetailView/taskMember/TaskMember";
+import TaskTodoAdd from "@/components/taskDetailView/taskTodoAdd/TaskTodoAdd";
 
 type Props = {
     currentTaskId: string;
@@ -40,11 +39,9 @@ function TaskDetailView({currentTaskId}: Props) {
             {/* START :  TO-DO & NOTE 목록 */}
             <section className='w-full flex-col space-y-4 '>
                 <div className='header-lg mx-2 items-center border-b border-neutral pb-2'>업무</div>
-                {/* 업무 header */}
                 <section className='flex-col space-y-4 px-2'>  {/* 업무 contents */}
                     <TaskTodoFilter/>
-                    <TaskTodoAddForm/>
-                    <TaskTodoAddButton/>
+                    <TaskTodoAdd/>
                 </section>
             </section>
             {/* END :  TO-DO 목록 */}
---
+++ src/components/taskDetailView/taskTodoAdd/TaskTodoAdd.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {RiAddLine} from "react-icons/ri";
+import TaskTodoAddForm from "@/components/taskDetailView/taskTodoAdd/TaskTodoAddForm";
+
+function TaskTodoAdd() {
+
+    return (
+        <>
+            <TaskTodoAddForm/>
+            <div className='pl-16'>
+                <button
+                    className='w-full flex items-center space-x-1 py-2 px-1 text-[1.65rem] leading-[2.15rem] text-neutral-dark hoverColorChange-neutral-light-1'>
+                    <RiAddLine/>
+                    <span>업무 목록 추가</span>
+                </button>
+            </div>
+        </>
+    );
+}
+
+export default TaskTodoAdd;
