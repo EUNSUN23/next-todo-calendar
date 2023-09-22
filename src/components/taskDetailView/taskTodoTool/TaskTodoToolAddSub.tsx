0e0d8849bbce1094975e63be7d672d53e73866a0
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-22 14:09:02 +0900

    Refactor: 업무입력 form 좌측 사이드툴 기능 -> TaskTodoToolAddSub, TaskTodoToolControlTodo 컴포넌트 분리

---
+++ src/components/taskDetailView/taskTodoTool/TaskTodoToolAddSub.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {IoMdAdd} from "react-icons/io";
+
+function TaskTodoToolAddSub(props) {
+    return (
+        <button className='hoverColorChange-neutral-light-1'>
+            <IoMdAdd/>
+        </button>
+    );
+}
+
+export default TaskTodoToolAddSub;
---
+++ src/components/taskDetailView/taskTodoTool/TaskTodoToolControlTodo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {PiDotsSixVerticalBold} from "react-icons/pi";
+
+function TaskTodoToolControlTodo() {
+    return (
+        <button className='hoverColorChange-neutral-light-1'>
+            <PiDotsSixVerticalBold/>
+        </button>
+    );
+}
+
+export default TaskTodoToolControlTodo;
