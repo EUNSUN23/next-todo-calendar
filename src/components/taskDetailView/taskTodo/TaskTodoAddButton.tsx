f989ebd359e54214474e67464ff745f54ca96763
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-26 20:11:42 +0900

    Feature(TaskTodoAddButton.tsx): 업무목록 버튼 클릭시 부모컴포넌트의 showTaskTodoAddElement 상태값 true로 만드는 onClickHandler 연결

--- src/components/taskDetailView/taskTodo/TaskTodoAddButton.tsx
+++ src/components/taskDetailView/taskTodo/TaskTodoAddButton.tsx
@@ -2,12 +2,15 @@
 import React from 'react';
 import {RiAddLine} from "react-icons/ri";
 
-
-function TaskTodoAddButton() {
+type Props = {
+    onClickHandler:() => void;
+}
+function TaskTodoAddButton({onClickHandler}:Props) {
 
     return (
         <div className='pl-16'>
             <button
+                onClick={onClickHandler}
                 className='w-full flex items-center space-x-1 py-2 px-1 text-[1.65rem] leading-[2.15rem] text-neutral-dark hoverColorChange-neutral-light-1'>
                 <RiAddLine/>
                 <span>업무 목록 추가</span>
