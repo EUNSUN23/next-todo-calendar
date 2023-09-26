08d51eeedea4050f803f7eb4748550296800ac11
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-26 20:14:45 +0900

    Feature: taskTodo 생성 & 반영 소스 완성

--- src/components/taskDetailView/taskTodo/TaskTodo.tsx
+++ src/components/taskDetailView/taskTodo/TaskTodo.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import TaskTodoList from "@/components/taskDetailView/taskTodo/TaskTodoList";
 import TaskTodoFilter from "@/components/taskDetailView/taskTodo/TaskTodoFilter";
-import TaskTodoAddButton from "@/components/taskDetailView/taskTodo/TaskTodoAddButton";
 import {useTaskTodo} from "@/hooks/useTaskTodo";
+import {TaskTodoItem} from "@/components/taskDetailView/taskTodo/TaskTodoItem";
 
 type Props = {
     taskId: string;
@@ -14,13 +14,19 @@ function TaskTodo({taskId}: Props) {
     // todo - loader 추가
     if(isLoading) return <div>loading...</div>
 
+    console.log("todos:::: ",todos);
+
+
     return (
         <section className='w-full flex-col space-y-4 '>
             <section className='flex-col space-y-4 px-2'>  {/* 업무 contents */}
                 <div className='header-lg mx-2 items-center border-b border-neutral pb-2'>업무</div>
                 <TaskTodoFilter/>
-                <TaskTodoList todos={todos}/>
-                <TaskTodoAddButton/>
+                <TaskTodoList>
+                    <div className='flex-col'>
+                        {todos?.map((v) => <TaskTodoItem key={v._id} todo={v} taskId={taskId}/>)}
+                    </div>
+                </TaskTodoList>
             </section>
         </section>
     );
--- src/components/taskDetailView/taskTodo/TaskTodoList.tsx
+++ src/components/taskDetailView/taskTodo/TaskTodoList.tsx
@@ -1,19 +1,78 @@
-import React from 'react';
-import {Todo} from "@/utils/types";
-import TaskTodoItem from "@/components/taskDetailView/taskTodo/TaskTodoItem";
-import TaskTodoFilter from "@/components/taskDetailView/taskTodo/TaskTodoFilter";
-import TaskTodoAddButton from "@/components/taskDetailView/taskTodo/TaskTodoAddButton";
+'use client';
+import React, {useState} from 'react';
+import {ReactChildNode, TaskTodoRequestVo} from "@/utils/types";
+import {useCreateTaskTodo} from "@/hooks/useCreateTaskTodo";
+import TaskTodoTool from "@/components/taskDetailView/taskTodo/TaskTodoTool";
+import CustomCheckbox from "@/components/ui/CustomCheckbox";
+import CustomInput from "@/components/ui/CustomInput";
+import {RiAddLine} from "react-icons/ri";
+import {formatDateToStr} from "@/utils/common";
+import {DateFormat} from "@/utils/constant";
+import Todo from "../../../../sanity-studio/schemas/todo";
+import {useOpenTask} from "@/context/OpenTaskContext";
 
-type Props = {
-    todos: Todo[] | []
-}
 
 // todo - note는 todo에 속하게 둘건지, 따로 둘건지 ?
-function TaskTodoList({todos}: Props) {
-    console.log("todos: ", todos);
+function TaskTodoList({children}: ReactChildNode) {
+    const {currentTaskId} = useOpenTask();
+    const {createTaskTodo} = useCreateTaskTodo();
+    const [showTaskTodoAddElement, setShowTaskTodoAddElement] = useState(false);
+    const [showTodoTool, setShowTodoTool] = useState(false);
+    const [todoContents, setTodoContents] = useState('');
+
+    function onChangeTodoInputHandler(value: string) {
+        setTodoContents(value);
+    }
+
+    function onBlurTaskTodoItemAddHandler() {
+        const today = formatDateToStr(new Date, DateFormat.YMD_DASH);
+        const user = { // todo - 실제 user 객체 넣을것
+            _id: 'testUser',
+            name: 'EUNSUN',
+            userName: 'esKim',
+            phone: '010111111111',
+            createdAt: today
+        }
+
+        const todo = {
+            groupId: currentTaskId
+            , contents: todoContents
+            , createdBy: user
+            , updateDate: today
+        } as Todo;
+
+        createTaskTodo({groupId: currentTaskId, todo} as TaskTodoRequestVo);
+        setShowTaskTodoAddElement(false);
+        setTodoContents('');
+    }
+
     return (
         <div className='flex-col'>
-            {todos?.map((v) => <TaskTodoItem key={v._id} todo={v}/>)}
+            {
+                showTaskTodoAddElement &&
+                <div
+                    className='pl-16 relative'
+                    onMouseOver={() => setShowTodoTool(true)}
+                    onMouseLeave={() => setShowTodoTool(false)}
+                    onBlur={onBlurTaskTodoItemAddHandler}
+                >
+                    {showTodoTool && <TaskTodoTool/>}
+                    <div
+                        className='w-full flex space-x-4 items-center text-3xl mx-3 p-1 hoverColorChange-neutral-light-1'>
+                        <CustomCheckbox disabled={true}/>
+                        <CustomInput todoContents={todoContents} onChangeHandler={onChangeTodoInputHandler}/>
+                    </div>
+                </div>
+            }
+            {children}
+            <div className='pl-16'>
+                <button
+                    onClick={() => setShowTaskTodoAddElement(true)}
+                    className='w-full flex items-center space-x-1 py-2 px-1 text-[1.65rem] leading-[2.15rem] text-neutral-dark hoverColorChange-neutral-light-1'>
+                    <RiAddLine/>
+                    <span>업무 목록 추가</span>
+                </button>
+            </div>
         </div>
     );
 }
