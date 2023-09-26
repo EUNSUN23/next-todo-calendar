69117a42b0229ab33aadde936aa15576648415c7
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-26 20:06:56 +0900

    Fix(CustomInput.tsx): todoContents, onChangeHandler prop으로 전달받도록 수정

--- src/components/ui/CustomInput.tsx
+++ src/components/ui/CustomInput.tsx
@@ -2,8 +2,12 @@
 import React, {useState} from 'react';
 import {BiWindow} from "react-icons/bi";
 
-function CustomInput() {
-    const [textValue, setInputTextValue] = useState('');
+type Props = {
+    todoContents:string;
+    onChangeHandler:(value:string) => void;
+};
+
+function CustomInput({todoContents, onChangeHandler}:Props) {
     const [placeholder, setPlaceholder] = useState('업무 입력');
 
 
@@ -14,17 +18,18 @@ function CustomInput() {
                 placeholder={placeholder}
                 className='relative ml-3 bg-transparent outline-none z-10 py-1'
                 onChange={(e) => {
-                    setInputTextValue(e.target.value);
+                    onChangeHandler(e.target.value);
                     if(e.target.value === "") setPlaceholder('업무 입력');
                 }}
                 spellCheck={false}
+                value={todoContents}
             />
 
             <div className='flex items-center absolute top-0 left-2 z-0 space-x-1'>
                     <div className={`flex px-1 pb-1`}>
-                        <div className='whitespace-nowrap text-transparent' >{textValue}</div>
+                        <div className='whitespace-nowrap text-transparent' >{todoContents}</div>
                     </div>
-                {textValue.length > 0 && <BiWindow size={22} className='cursor-pointer opacity-0 hover:opacity-100 transition opacity ease-in-out duration-100'/>}
+                {todoContents.length > 0 && <BiWindow size={22} className='cursor-pointer opacity-0 hover:opacity-100 transition opacity ease-in-out duration-100'/>}
             </div>
         </div>
     );
