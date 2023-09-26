d38d0971fd431eb8ceebfa56997e89dba58ea84e
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-26 15:35:08 +0900

    Refactor: TaskTodoFilter, TaskTodoList, TaskTodoAddButton을 TaskTodo컴포넌트 로 합침

--- src/components/taskDetailView/taskTodo/TaskTodo.tsx
+++ src/components/taskDetailView/taskTodo/TaskTodo.tsx
@@ -12,8 +12,8 @@ function TaskTodo({todos}: Props) {
     return (
         <section className='w-full flex-col space-y-4 '>
             <section className='flex-col space-y-4 px-2'>  {/* 업무 contents */}
-                <TaskTodoFilter/>
                 <div className='header-lg mx-2 items-center border-b border-neutral pb-2'>업무</div>
+                <TaskTodoFilter/>
                 <TaskTodoList todos={todos}/>
                 <TaskTodoAddButton/>
             </section>
