e92a431b0e16429f40cd52da2ce7c87fd1cc3942
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-26 15:28:01 +0900

    Fix(TaskTodoList.tsx): todo목록만 props로 받게끔 수정

--- src/components/taskDetailView/taskTodo/TaskTodoList.tsx
+++ src/components/taskDetailView/taskTodo/TaskTodoList.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import {Note, Todo} from "@/utils/types";
+import {Todo} from "@/utils/types";
 import TaskTodoItem from "@/components/taskDetailView/taskTodo/TaskTodoItem";
 import TaskTodoFilter from "@/components/taskDetailView/taskTodo/TaskTodoFilter";
 import TaskTodoAddButton from "@/components/taskDetailView/taskTodo/TaskTodoAddButton";
 
 type Props = {
-    todos: Todo[] | [],
-    notes: Note[] | []
+    todos: Todo[] | []
 }
 
 // todo - note는 todo에 속하게 둘건지, 따로 둘건지 ?
-function TaskTodoList({todos, notes}: Props) {
+function TaskTodoList({todos}: Props) {
     console.log("todos: ", todos);
-    console.log("notes: ", notes);
     return (
         <section className='flex-col space-y-4 px-2'>  {/* 업무 contents */}
             <TaskTodoFilter/>
