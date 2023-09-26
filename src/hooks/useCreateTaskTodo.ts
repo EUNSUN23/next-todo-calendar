21bb6f85483b77e328b7ad139ad19beea8c81c40
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-26 20:05:02 +0900

    Fix(useCreateTaskTodo.ts): 버그 수정
    1. mutate함수명 수정
    2. mutate후 기존 캐시 지우도록 수정

--- src/hooks/useCreateTaskTodo.ts
+++ src/hooks/useCreateTaskTodo.ts
@@ -1,15 +1,20 @@
 'use client';
 
-import {useMutation} from "@tanstack/react-query";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {toast} from "react-toastify";
 import {TaskTodoRequestVo} from "@/utils/types";
+import {createEditTaskTodo as createTaskTodoApi} from "@/service/task";
 
 
 export function useCreateTaskTodo(){
+    const queryClient = useQueryClient();
     const {mutate: createTaskTodo, isLoading:isCreating} = useMutation({
         mutationFn: async (requestVo:TaskTodoRequestVo) => {
             toast.loading('Todo 생성 중입니다.');
-            await createTaskTodo(requestVo);
+            await createTaskTodoApi(requestVo);
+            queryClient.invalidateQueries({
+                queryKey:['taskTodo']
+            });
         } ,
         onSuccess:() => {
             toast.dismiss();
