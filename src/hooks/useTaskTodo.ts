938ef6e731da072002ad04501c2aceec5c37d006
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-26 17:15:33 +0900

    Fix: TaskTodo조회 파라미터 수정

--- src/hooks/useTaskTodo.ts
+++ src/hooks/useTaskTodo.ts
@@ -2,6 +2,7 @@
 
 import {Todo} from "@/utils/types";
 import {useQuery} from "@tanstack/react-query";
+import {getTaskTodoById} from "@/service/task";
 
 type TaskTodoResponse = {
     todos : Todo[],
@@ -11,7 +12,7 @@ type TaskTodoResponse = {
 export function useTaskTodo(taskId:string):TaskTodoResponse{
     const {data:todos, isLoading, error} = useQuery({
         queryKey:['taskTodo'],
-        queryFn: () => getTaskTodoById(taskId)
+        queryFn: () => getTaskTodoById({groupId:taskId})
     });
 
     return {todos, isLoading, error} as TaskTodoResponse;
