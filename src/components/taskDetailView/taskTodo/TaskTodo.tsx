57e963a922a58a7384aa6c3d28c3b36aa20038e8
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-14 20:47:47 +0900

    Design(TaskTodo.tsx): px 삭제

--- src/components/taskDetailView/taskTodo/TaskTodo.tsx
+++ src/components/taskDetailView/taskTodo/TaskTodo.tsx
@@ -19,7 +19,7 @@ function TaskTodo({taskId}: Props) {
 
     return (
         <section className='w-full flex-col space-y-4 '>
-            <section className='flex-col px-2'>  {/* 업무 contents */}
+            <section className='flex-col'>  {/* 업무 contents */}
                 <div className='h-14 header-lg mx-2 items-center border-b border-neutral pb-2'>업무</div>
                 <TaskTodoFilter/>
                 <TaskTodoList>
