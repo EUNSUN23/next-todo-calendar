8bfbf1cc1e5bcfb55a19a2368d4dac9048761bb4
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-14 16:45:04 +0900

    Refactor(TaskTodoAddItem.tsx): input영역 TaskTodoAddItem.tsx 으로 대체

--- src/components/taskDetailView/taskTodo/TaskTodoAddItem.tsx
+++ src/components/taskDetailView/taskTodo/TaskTodoAddItem.tsx
@@ -2,14 +2,13 @@
 
 import React, {Dispatch, SetStateAction, useState} from 'react';
 import TaskTodoTool from "@/components/taskDetailView/taskTodo/TaskTodoTool";
-import CustomCheckbox from "@/components/ui/CustomCheckbox";
-import CustomInput from "@/components/ui/CustomInput";
 import {formatDateToStr} from "@/utils/common";
 import {DateFormat} from "@/utils/constant";
 import Todo from "../../../../sanity-studio/schemas/todo";
 import {TaskTodoRequestVo} from "@/utils/types";
 import {useOpenTask} from "@/context/OpenTaskContext";
 import {useCreateTaskTodo} from "@/hooks/useCreateTaskTodo";
+import TaskTodoAddInput from "@/components/taskDetailView/taskTodo/TaskTodoAddInput";
 
 interface TaskTodoAddItemProps {
     setIsOpen:Dispatch<SetStateAction<boolean>>;
@@ -51,11 +50,7 @@ function TaskTodoAddItem({setIsOpen}:TaskTodoAddItemProps) {
             onBlur={onBlurHandler}
         >
             {showTodoTool && <TaskTodoTool/>}
-            <div
-                className='w-full flex space-x-4 items-center text-3xl mx-3 p-1 hoverColorChange-neutral-light-1'>
-                <CustomCheckbox disabled={true}/>
-                <CustomInput todoContents={todoContents} onChangeHandler={(v:string) => setTodoContents(v)}/>
-            </div>
+            <TaskTodoAddInput todoContents={todoContents} onChangeHandler={(v:string) => setTodoContents(v)} isCheckDisabled={true}/>
         </div>
     );
 }
