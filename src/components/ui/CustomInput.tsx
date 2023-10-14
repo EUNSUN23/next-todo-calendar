0a2a648039d5de0bb4eee4be7598d254bc2004a2
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-14 16:40:27 +0900

    Fix(CustomInput.tsx): window 아이콘 제거

--- src/components/ui/CustomInput.tsx
+++ src/components/ui/CustomInput.tsx
@@ -1,13 +1,13 @@
 'use client';
 import React, {useState} from 'react';
-import {BiWindow} from "react-icons/bi";
 
 type Props = {
-    todoContents:string;
-    onChangeHandler:(value:string) => void;
+    todoContents: string;
+    onChangeHandler?: (value: string) => void;
+    isReadOnly?: boolean;
 };
 
-function CustomInput({todoContents, onChangeHandler}:Props) {
+function CustomInput({todoContents, onChangeHandler, isReadOnly = false}: Props) {
     const [placeholder, setPlaceholder] = useState('업무 입력');
 
 
@@ -25,12 +25,6 @@ function CustomInput({todoContents, onChangeHandler}:Props) {
                 value={todoContents}
             />
 
-            <div className='flex items-center absolute top-0 left-2 z-0 space-x-1'>
-                    <div className={`flex px-1 pb-1`}>
-                        <div className='whitespace-nowrap text-transparent' >{todoContents}</div>
-                    </div>
-                {todoContents.length > 0 && <BiWindow size={22} className='cursor-pointer opacity-0 hover:opacity-100 transition opacity ease-in-out duration-100'/>}
-            </div>
         </div>
     );
 }
