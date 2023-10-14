770ffbef85ae526f63f86ecc615b2aedec5c18f4
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-14 16:38:20 +0900

    Fix(TaskTodoInput.tsx): 수정
    1. 초기 todo내용만 props로 전달받고, 내부에서 todoContents 상태 관리하도록 수정
    
    2. windowButton을 자식 컴포넌트로 받도록 수정
    
    3. checked, isReadOnly state 추가

--- src/components/taskDetailView/taskTodo/TaskTodoInput.tsx
+++ src/components/taskDetailView/taskTodo/TaskTodoInput.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+'use client';
+
+import React, {ChangeEvent, useState} from 'react';
 import CustomCheckbox from "@/components/ui/CustomCheckbox";
 import CustomInput from "@/components/ui/CustomInput";
 
 interface TaskTodoInputProps {
-    todoContents:string;
-    onChangeHandler: (v:string) => void;
+    initContents: string;
+    children:React.ReactElement;
 }
-function TaskTodoInput({todoContents, onChangeHandler}:TaskTodoInputProps) {
+function TaskTodoInput({initContents, children}:TaskTodoInputProps) {
+    const [todoContents, setTodoContents] = useState(() => initContents);
+    const [checked, setChecked] = useState(false);
+    const [isReadOnly, setIsReadOnly] = useState(false);
+
+    function onChangeCheckboxHandler(e:ChangeEvent<HTMLInputElement>){
+        console.log("e.target.checked: ",e.target.checked);
+        console.log("check!!!")
+        setChecked(e.target.checked);
+    }
+
     return (
         <div
-            className='w-full flex space-x-4 items-center text-3xl mx-3 p-1 hoverColorChange-neutral-light-1'>
-            <CustomCheckbox disabled={true}/>
-            <CustomInput todoContents={todoContents} onChangeHandler={onChangeHandler}/>
+            className='w-full flex items-center text-3xl p-1 hoverColorChange-neutral-light-1'>
+            <CustomCheckbox checked={checked} onChangeHandler={onChangeCheckboxHandler}  disabled={false}/>
+            <CustomInput todoContents={todoContents} onChangeHandler={(v:string) => setTodoContents(v)} isReadOnly={isReadOnly} />
+            {children}
         </div>
     );
 }
