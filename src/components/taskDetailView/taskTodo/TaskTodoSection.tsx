210c13c2fa7069e3ab314cbdd227119223b36e47
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-17 20:30:06 +0900

    Fix(TaskTodoSection.tsx) : Tabs에 내려주는 테스트 데이터 삭제

--- src/components/taskDetailView/taskTodo/TaskTodoSection.tsx
+++ src/components/taskDetailView/taskTodo/TaskTodoSection.tsx
@@ -6,19 +6,11 @@ import Tabs from "@/components/ui/Tabs/Tabs";
 
 function TaskTodoSection() {
 
-    const tabInfoList = [
-        {
-            name:'task1',
-            href:'/main/task1 description/task1'
-        }
-    ]
-
-
     return (
         <section className='w-full flex-col space-y-4 '>
             <section className='flex-col'>  {/* 업무 contents */}
                 {/*<div className='h-14 header-lg mx-2 items-center border-b border-neutral pb-2'>업무</div>*/}
-                <Tabs tabInfoList={tabInfoList}/>
+                <Tabs />
                 <TaskTodoFilter/>
                 <TaskTodoList/>
             </section>
