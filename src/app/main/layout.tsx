85fc632ff20432e51d807e7d73152ca3d3724f34
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-17 15:18:14 +0900

    Fix(MainLayout): currentTask가 default(빈 값)이 아닐때에만 업무 디테일 뷰 적용되도록 수정

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -5,18 +5,19 @@ import TaskView from "@/components/TaskView";
 import TaskDetailView from "@/components/taskDetailView/TaskDetailView";
 import {useRecoilState} from "recoil";
 import {currentTaskStateStore} from "@/store/CurrentTaskStateStore";
+import {DefaultTask} from "@/utils/DefaultTask";
 
 function MainLayout() {
-    const [currentTask, setCurrentTask] = useRecoilState(currentTaskStateStore);
+    const [{currentTask}, setCurrentTask] = useRecoilState(currentTaskStateStore);
 
     const fullDisplay = "xl:basis-[850px] flex max-lg:flex-col";
     const todoDisplay = "lg:basis-[400px] md:basis-[250px]";
 
     return (
         <>
-            <section className={`basis-full ${currentTask ? fullDisplay : todoDisplay} `}>
+            <section className={`basis-full ${currentTask instanceof DefaultTask ? fullDisplay : todoDisplay} `}>
                 <TaskView/>
-                <TaskDetailView currentTask={currentTask}/>
+                {!(currentTask instanceof DefaultTask) && <TaskDetailView />}
             </section>
             <CalendarSection/>
         </>
