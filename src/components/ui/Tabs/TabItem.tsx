a59fc540ba915c666574867105eeb75c6d9dec00
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-18 11:11:31 +0900

    Design: taskTodo 상세 탭 css 수정

--- src/components/ui/Tabs/TabItem.tsx
+++ src/components/ui/Tabs/TabItem.tsx
@@ -8,9 +8,10 @@ import {
 } from "@/store/CurrentTaskTodoStateStore";
 import {useRecoilValue, useResetRecoilState, useSetRecoilState} from "recoil";
 import {useQueryClient} from "@tanstack/react-query";
+import {AiOutlineRight} from "react-icons/ai";
 
 
-function TabItem({name, id}:TaskTodoTab) {
+function TabItem({name, id}: TaskTodoTab) {
     const currentTaskTodoTab = useRecoilValue(currentTaskTodoTabSelector);
     const setCurrentTaskTodo = useSetRecoilState(currentTaskTodoStateStore);
     const resetCurrentTaskTodo = useResetRecoilState(currentTaskTodoStateStore);
@@ -20,8 +21,8 @@ function TabItem({name, id}:TaskTodoTab) {
 
     const dynamicClass = currentTaskTodoTab.id === id ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700';
 
-    function onClickTabItemHandler(){
-        if(id === 'taskTodoList') {
+    function onClickTabItemHandler() {
+        if (id === 'taskTodoList') {
             resetCurrentTaskTodo();
             resetTaskTodoTabList();
             return;
@@ -29,7 +30,7 @@ function TabItem({name, id}:TaskTodoTab) {
 
         const todos = queryClient.getQueryState(['taskTodo']);
         const currentTaskTodo = todos?.data.find(v => v._id === id);
-        setCurrentTaskTodo({currentTaskTodoId:id, currentTaskTodo});
+        setCurrentTaskTodo({currentTaskTodoId: id, currentTaskTodo});
     }
 
     return (
@@ -37,7 +38,16 @@ function TabItem({name, id}:TaskTodoTab) {
             onClick={onClickTabItemHandler}
             className={`${dynamicClass} group inline-flex items-center border-b-2 py-4 px-1 text-3xl font-medium`}
         >
-            <span>{name}</span>
+            <span>
+                {
+                    id === 'taskTodoList' ? name
+                        : <span className='flex items-center'>
+                            업무 목록
+                            <AiOutlineRight className='mx-2'/>
+                            {name}
+                    </span>
+                }
+            </span>
         </div>
     );
 }
