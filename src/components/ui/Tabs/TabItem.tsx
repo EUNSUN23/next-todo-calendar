abd609affd3b81c9e94677721625b7489ab1619d
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-18 10:55:57 +0900

    Fix(TabItem): '업무 목록' tab 클릭시 tabList, currrentTaskTodo 상태 default값으로 초기화해서 tab 닫히도록 수정

--- src/components/ui/Tabs/TabItem.tsx
+++ src/components/ui/Tabs/TabItem.tsx
@@ -1,19 +1,32 @@
 'use client';
 import React from 'react';
-import {currentTaskTodoStateStore, currentTaskTodoTabSelector, TaskTodoTab} from "@/store/CurrentTaskTodoStateStore";
-import {useRecoilValue, useSetRecoilState} from "recoil";
+import {
+    currentTaskTodoStateStore,
+    currentTaskTodoTabSelector,
+    TaskTodoTab,
+    taskTodoTabListStateStore
+} from "@/store/CurrentTaskTodoStateStore";
+import {useRecoilValue, useResetRecoilState, useSetRecoilState} from "recoil";
 import {useQueryClient} from "@tanstack/react-query";
 
 
 function TabItem({name, id}:TaskTodoTab) {
     const currentTaskTodoTab = useRecoilValue(currentTaskTodoTabSelector);
     const setCurrentTaskTodo = useSetRecoilState(currentTaskTodoStateStore);
+    const resetCurrentTaskTodo = useResetRecoilState(currentTaskTodoStateStore);
+    const resetTaskTodoTabList = useResetRecoilState(taskTodoTabListStateStore);
 
     const queryClient = useQueryClient();
 
     const dynamicClass = currentTaskTodoTab.id === id ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700';
 
     function onClickTabItemHandler(){
+        if(id === 'taskTodoList') {
+            resetCurrentTaskTodo();
+            resetTaskTodoTabList();
+            return;
+        }
+
         const todos = queryClient.getQueryState(['taskTodo']);
         const currentTaskTodo = todos?.data.find(v => v._id === id);
         setCurrentTaskTodo({currentTaskTodoId:id, currentTaskTodo});
