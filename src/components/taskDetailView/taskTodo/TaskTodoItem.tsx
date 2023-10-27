1c1c5a8a3221f826e62210a2d15f5b7ac73c0ed9
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-27 13:52:24 +0900

    Fix(TaskTodoItem): TaskTodoTool로 마우스 이동시 currentTaskTodo 초기화하지 않도록 수정

--- src/components/taskDetailView/taskTodo/TaskTodoItem.tsx
+++ src/components/taskDetailView/taskTodo/TaskTodoItem.tsx
@@ -4,15 +4,9 @@ import {Todo} from "@/utils/types";
 import TaskTodoTool from "@/components/taskDetailView/taskTodo/TaskTodoTool";
 import TaskTodoInput from "@/components/taskDetailView/taskTodo/TaskTodoInput";
 import TaskTodoWindowButton from "@/components/taskDetailView/taskTodo/TaskTodoWindowButton";
-import {useRecoilState, useResetRecoilState} from "recoil";
+import {useRecoilState} from "recoil";
 import {currentTaskTodoStateStore} from "@/store";
 
-// TODO 1-2. dot 클릭시 다음 항목 나오게 하기 - 삭제, todo 할당하기, 타입 변경(todo <-> note 아이콘으로.)
-// TODO 2. + 클릭시 sub 업무/note form 생성
-// TODO 2-1. 업무목록 추가 버튼 클릭시 위로 form 추가.
-// TODO 4. 처음 상태는 input에 focus 된채로, focus 사라지면 그대로 현재 Task의 todo에 추가. (제목 없으면 '제목 없음' 으로)
-// TODO 5. 현재 task에 todo 생성(currentTaskId이용) 로직 - groupId(currTaskid), contents(not require), createdBy, updateDate, assignee(user)
-// TODO 6. 수정후에는 마우스 hover시
 
 type Props = {
     todo: Todo;
@@ -20,27 +14,20 @@ type Props = {
 
 export function TaskTodoItem({todo}: Props) {
     const [{isOnHover, currentTaskTodoId}, setCurrentTaskTodo] = useRecoilState(currentTaskTodoStateStore);
-    const resetCurrentTaskTodo = useResetRecoilState(currentTaskTodoStateStore);
     const showTools = isOnHover && todo._id === currentTaskTodoId;
 
     function onMouseOverHandler(e) {
         e.stopPropagation();
-        setCurrentTaskTodo({currentTaskTodoId:todo._id!, currentTaskTodo:todo, isOnHover:true});
+        setCurrentTaskTodo({currentTaskTodoId: todo._id!, currentTaskTodo: todo, isOnHover: true});
 
     }
 
-    function onMouseLeaveHandler(e) {
-        e.stopPropagation();
-        resetCurrentTaskTodo();
-    }
-
-    const windowButton =  showTools ? <TaskTodoWindowButton todo={todo}/> : <></>;
+    const windowButton = showTools ? <TaskTodoWindowButton todo={todo}/> : <></>;
 
     return (
         <div
-            className='flex items-center pl-16 relative'
+            className='w-full flex items-center pl-16 relative'
             onMouseOver={onMouseOverHandler}
-            onMouseLeave={onMouseLeaveHandler}
         >
             {showTools && <TaskTodoTool/>}
             <TaskTodoInput initContents={todo?.contents || ''}>
@@ -49,4 +36,3 @@ export function TaskTodoItem({todo}: Props) {
         </div>
     );
 }
-
