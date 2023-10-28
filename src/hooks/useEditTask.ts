c6b03e9da2ce304dafefc2d450297b312fcbafbb
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-28 14:04:26 +0900

    Fix(useEditTask): task 정보 수정하고 쿼리 캐시 날려도 리코일 상태를 수정해주지 않아서 화면에 반영 안되던 버그 수정

--- src/hooks/useEditTask.ts
+++ src/hooks/useEditTask.ts
@@ -4,23 +4,28 @@ import {editTaskById as editTaskByIdApi} from "@/service/task";
 import {toast, Zoom} from "react-toastify";
 import {useRef} from "react";
 import 'react-toastify/dist/ReactToastify.css';
+import {useSetRecoilState} from "recoil";
+import {currentTaskStateStore} from "@/store";
 
 
 export function useEditTask() {
     const queryClient = useQueryClient();
+    const setCurrentTask = useSetRecoilState(currentTaskStateStore);
 
     const editToastId = useRef('');
 
     const {isLoading: isEditing, mutate: editTaskById, error} = useMutation({
         mutationFn: async (requestVo: TaskEditRequestVo) => {
             editToastId.current = toast.loading("Task 수정 중입니다.",{transition:Zoom, theme:'light'}) as string;
-            await editTaskByIdApi(requestVo);
+            const res = await editTaskByIdApi(requestVo);
             toast.dismiss(editToastId.current);
+            return res;
         },
-        onSuccess: async () => {
+        onSuccess: async (data) => {
             await queryClient.invalidateQueries({ // 캐시데이터 invalidate함으로써 데이터 refetch하도록.
                 queryKey: ['task']
             });
+            setCurrentTask({currentTaskId:data._id, currentTask:data});
             toast.dismiss(editToastId.current);
         },
         onError: err => {
